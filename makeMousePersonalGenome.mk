fa5baafd64f54046801753410d4f81381df8958b
shaopei <sc2457@cornell.edu>
2017-09-05 15:37:11 -0400

    make dipolid genome for F1 mouse. modified for alleleDB pipeline

--- makeMousePersonalGenome.mk
+++ makeMousePersonalGenome.mk
@@ -1,4 +1,4 @@
-USAGE := "make -f makePersonalGenome.mk DATA_DIR=/path/to/your/VCFdir OUTPUT_SAMPLE_NAME=NA12878 MAT_SAMPLE_NAME=mat PAT_SAMPLE_NAME=pat FILE_NAME_BAM=filename.in.DATA_DIR.bam FILE_NAME_VCF=filename.in.DATA_DIR.vcf"
+USAGE := "make -f makePersonalGenome.mk DATA_DIR=/path/to/your/VCFdir OUTPUT_SAMPLE_NAME=NA12878 MAT_SAMPLE_NAME=mat AT_SAMPLE_NAME=pat FILE_NAME_BAM=filename.in.DATA_DIR.bam FILE_NAME_VCF=filename.in.DATA_DIR.vcf"
 
 ##
 ## Define fixed, experiment-wide analysis parameters
@@ -49,6 +49,8 @@ $(OUTPUT_DIR):
 ##
 $(OUTPUT_DIR)/maternal.chain:$(OUTPUT_DIR) 
 	$(JAVA_EXE) -Xmx$(MAX_RAM) -jar $(EXE_DIR)/vcf2diploid.jar -id $(VCF_sampleID) -pass -chr $(FASTA_PATH) -vcf $(DATA_DIR)/$(FILE_NAME_VCF) -outDir $(OUTPUT_DIR) >& $(OUTPUT_DIR)/vcf2diploid.log
+	cat $(OUTPUT_DIR)/maternal.chain | awk ' ($$1 == "chain") {print $$1,$$2,$$8,$$9,$$10,$$11,$$12,$$3,$$4,$$5,$$6,$$7,$$13}; ($$1 != "chain"){print $$1, $$3, $$2 }'>  $(OUTPUT_DIR)/mat2ref.chain
+	cat $(OUTPUT_DIR)/paternal.chain | awk ' ($$1 == "chain") {print $$1,$$2,$$8,$$9,$$10,$$11,$$12,$$3,$$4,$$5,$$6,$$7,$$13}; ($$1 != "chain"){print $$1, $$3, $$2 }'>  $(OUTPUT_DIR)/pat2ref.chain
 
 ##
 ## Build Maternal genome
@@ -68,9 +70,11 @@ $(OUTPUT_DIR)/bowtie_build.paternal.log: $(OUTPUT_DIR)/bowtie_build.maternal.log
 ## Count reads in each snp region
 ##
 $(OUTPUT_DIR)/$(OUTPUT_SAMPLE_NAME).snp.bed.counts: $(OUTPUT_DIR)/bowtie_build.paternal.log
-	$(EXE_DIR)/vcf2snp -c $(VCF_sampleID) -m $(MAT_SAMPLE_NAME) -d $(PAT_SAMPLE_NAME) -p 1 -r 1 -s $(VCF2SNP_SNP_INDEL) $(DATA_DIR)/$(FILE_NAME_VCF) > $(OUTPUT_DIR)/$(OUTPUT_SAMPLE_NAME).snp
-	cat $(OUTPUT_DIR)/$(OUTPUT_SAMPLE_NAME).snp | awk '{print $$1"\t"($$2-1000)"\t"($$2+1000)"\t"$$1"_"($$2-1000)"_"($$2+1000)}' | grep -v "-" > $(OUTPUT_DIR)/$(OUTPUT_SAMPLE_NAME).snp.bed
-	intersectBed -sorted -wo -bed -a $(DATA_DIR)/$(FILE_NAME_BAM) -b $(OUTPUT_DIR)/$(OUTPUT_SAMPLE_NAME).snp.bed | awk '{print $$16}' | sort | uniq -c > $(OUTPUT_DIR)/$(OUTPUT_SAMPLE_NAME).snp.bed.counts
+	$(EXE_DIR)/vcf2snp -c $(VCF_sampleID) -m $(MAT_SAMPLE_NAME) -d $(PAT_SAMPLE_NAME) -p 1 -r 1 -s $(VCF2SNP_SNP_INDEL) $(DATA_DIR)/$(FILE_NAME_VCF) > $(OUTPUT_DIR)/$(OUTPUT_SAMPLE_NAME).snp.unfiltered
+	python $(EXE_DIR)/Remove_snps_skipped_in_vcf2diploid.py $(OUTPUT_DIR)/vcf2diploid.log $(OUTPUT_DIR)/$(OUTPUT_SAMPLE_NAME).snp.unfiltered $(OUTPUT_DIR)/$(OUTPUT_SAMPLE_NAME).snp $(OUTPUT_DIR)/$(OUTPUT_SAMPLE_NAME).snp.skipped
+	cat $(OUTPUT_DIR)/$(OUTPUT_SAMPLE_NAME).snp | awk '{print $$1"\t"($$2-1000)"\t"($$2+1000)"\t"$$1"_"($$2-1000)"_"($$2+1000)}' | grep -v "-" > $(OUTPUT_DIR)/$(OUTPUT_SAMPLE_NAME).snp.bed.unsort
+	LC_ALL=C sort --temporary-directory=/workdir/sc2457/tmp/  --parallel=10 -k1,1 -k2,2n $(OUTPUT_DIR)/$(OUTPUT_SAMPLE_NAME).snp.bed.unsort > $(OUTPUT_DIR)/$(OUTPUT_SAMPLE_NAME).snp.bed
+	intersectBed -sorted -wo -bed -a $(DATA_DIR)/$(FILE_NAME_BAM) -b $(OUTPUT_DIR)/$(OUTPUT_SAMPLE_NAME).snp.bed | awk '{print $$16}' | LC_ALL=C sort --temporary-directory=/workdir/sc2457/tmp/  --parallel=10 | uniq -c > $(OUTPUT_DIR)/$(OUTPUT_SAMPLE_NAME).snp.bed.counts
 
 
 ##
@@ -78,15 +82,16 @@ $(OUTPUT_DIR)/$(OUTPUT_SAMPLE_NAME).snp.bed.counts: $(OUTPUT_DIR)/bowtie_build.p
 ##
 $(OUTPUT_DIR)/$(OUTPUT_SAMPLE_NAME).alleleSeqInput.snp: $(OUTPUT_DIR)/$(OUTPUT_SAMPLE_NAME).snp.bed.counts
 	cat $(OUTPUT_DIR)/$(OUTPUT_SAMPLE_NAME).snp.bed.counts | awk '{ total += $$1; count++ } END { print total/count }' > $(OUTPUT_DIR)/$(OUTPUT_SAMPLE_NAME).snp.bed.meancount
-	cat $(OUTPUT_DIR)/$(OUTPUT_SAMPLE_NAME).snp.bed.counts | sort -n -k 1 | awk '{ lines[NR]=$$0; } END { print lines[int(NR/2)+1] }' | awk '{print $$1}' > $(OUTPUT_DIR)/$(OUTPUT_SAMPLE_NAME).snp.bed.mediancount
+	cat $(OUTPUT_DIR)/$(OUTPUT_SAMPLE_NAME).snp.bed.counts | LC_ALL=C sort --temporary-directory=/workdir/sc2457/tmp/  --parallel=10 -n -k 1 | awk '{ lines[NR]=$$0; } END { print lines[int(NR/2)+1] }' | awk '{print $$1}' > $(OUTPUT_DIR)/$(OUTPUT_SAMPLE_NAME).snp.bed.mediancount
 	cat $(OUTPUT_DIR)/$(OUTPUT_SAMPLE_NAME).snp.bed.counts | awk '{getline avg<"$(OUTPUT_DIR)/$(OUTPUT_SAMPLE_NAME).snp.bed.mediancount"; print $$2"\t"$$1/avg }' > $(OUTPUT_DIR)/tmp.cnv
-	cat $(OUTPUT_DIR)/$(OUTPUT_SAMPLE_NAME).snp | awk '{print $$1"_"($$2-1000)"_"($$2+1000)"\t"$$0}' | sort -k 1 > $(OUTPUT_DIR)/tmp.snp
+	cat $(OUTPUT_DIR)/$(OUTPUT_SAMPLE_NAME).snp | awk '{print $$1"_"($$2-1000)"_"($$2+1000)"\t"$$0}' | LC_ALL=C sort --temporary-directory=/workdir/sc2457/tmp/  --parallel=10 -k 1 > $(OUTPUT_DIR)/tmp.snp
 	## Combine the snp data and the cnv data by region ID
 	awk 'NR==FNR {h[$$1] = $$0; next} {print h[$$1]"\t"$$0}' $(OUTPUT_DIR)/tmp.snp $(OUTPUT_DIR)/tmp.cnv > $(OUTPUT_DIR)/$(OUTPUT_SAMPLE_NAME).snpANDcnv
 	## Create the snp and cnv input files for alleleseq
 	cat $(OUTPUT_DIR)/$(OUTPUT_SAMPLE_NAME).snpANDcnv | awk '{print $$2"\t"$$3"\t"$$4"\t"$$5"\t"$$6"\t"$$7"\t"$$8}' > $(OUTPUT_DIR)/$(OUTPUT_SAMPLE_NAME).alleleSeqInput.snp
 	echo -e "chrm\tsnppos\trd" > $(OUTPUT_DIR)/$(OUTPUT_SAMPLE_NAME).alleleSeqInput.cnv
 	cat $(OUTPUT_DIR)/$(OUTPUT_SAMPLE_NAME).snpANDcnv | awk '{print $$2"\t"$$3"\t"$$10}' >> $(OUTPUT_DIR)/$(OUTPUT_SAMPLE_NAME).alleleSeqInput.cnv
+	cat $(OUTPUT_DIR)/$(OUTPUT_SAMPLE_NAME).alleleSeqInput.snp | awk '{OFS="\t"}{FS="\t"}{print "chr"$$1, $$2-1, $$2, $$6 }' >  $(OUTPUT_DIR)/$(OUTPUT_SAMPLE_NAME).alleleDBInput.snp.bed
 	rm $(OUTPUT_DIR)/tmp.cnv
 	rm $(OUTPUT_DIR)/tmp.snp
 
